01c76bf4c132b082a273b2155eba088b366647fd
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-02-22 14:55:50 +0530

    Fix typo in programming languages section title and add additional programming languages

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,42 @@ function App() {
       <Hero />
       <section className="languages-section text-center">
         <div className="container">
-          <h3 className="section-title">Programin Lanugaes</h3>
+          <h3 className="section-title">Programing Languages</h3>
           <div className="row justify-content-center">
             <ProgramingL
               image={{
                 url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
                 lan: "Java",
+              }}
+            />
+            <ProgramingL
+              image={{
+                url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+                lan: "C#",
+              }}
+            />
+            <ProgramingL
+              image={{
+                url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
+                lan: "C",
+              }}
+            />
+            <ProgramingL
+              image={{
+                url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+                lan: "JavaScript",
+              }}
+            />
+            <ProgramingL
+              image={{
+                url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+                lan: "Html",
+              }}
+            />
+            <ProgramingL
+              image={{
+                url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+                lan: "CSS",
               }}
             />
           </div>
