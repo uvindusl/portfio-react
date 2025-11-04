1bebf713dfe922864356246061f81489788f9f00
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-11-05 02:03:30 +0530

    feat: add CI Pipline

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfio-react/",
-
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.js",
-    css: false,
+    css: true,
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "src/test/", "**/*.config.js"],
+      reporter: ["text", "json", "html", "lcov"],
+      exclude: [
+        "node_modules/",
+        "src/test/",
+        "**/*.config.js",
+        "**/*.config.ts",
+        "**/dist/**",
+        "**/.{idea,git,cache,output,temp}/**",
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
